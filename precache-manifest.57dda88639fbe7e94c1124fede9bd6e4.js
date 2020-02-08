75f94dac787c9228aa02049bbd8905d9992c0b47
SlickJacket <novenarii@gmail.com>
2020-02-08 12:09:00 -0500

    Updates

---
+++ precache-manifest.57dda88639fbe7e94c1124fede9bd6e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4187ee7a07a2f671a09491288ccf6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "76a0b6fa9ec3722b5099",
+    "url": "/portfolio/static/css/main.c7f11bee.chunk.css"
+  },
+  {
+    "revision": "205a805b084222a08234",
+    "url": "/portfolio/static/js/2.13314106.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/portfolio/static/js/2.13314106.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76a0b6fa9ec3722b5099",
+    "url": "/portfolio/static/js/main.0ece7e07.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "3181af4d75ae03e2208d7017eeaed3b0",
+    "url": "/portfolio/static/media/Melius1.3181af4d.png"
+  },
+  {
+    "revision": "f51de274a3c8b70fd8e3df868ff18e9e",
+    "url": "/portfolio/static/media/Melius1gf.f51de274.gif"
+  },
+  {
+    "revision": "31dd690bcd313e759f9c1cd9a2e9b9a9",
+    "url": "/portfolio/static/media/Melius_Logo2.31dd690b.png"
+  },
+  {
+    "revision": "c0642ff2212a35b0c580ce11bd860012",
+    "url": "/portfolio/static/media/goldteethusalogo.c0642ff2.png"
+  },
+  {
+    "revision": "ba5ea3f19a5fd1b994672f203aeb24d2",
+    "url": "/portfolio/static/media/gtusagf.ba5ea3f1.gif"
+  },
+  {
+    "revision": "38753e6c7e7472005bdd608182480d39",
+    "url": "/portfolio/static/media/heart.38753e6c.png"
+  },
+  {
+    "revision": "67ab1d6e8612e97f71602fbe965185e5",
+    "url": "/portfolio/static/media/melius2gf.67ab1d6e.gif"
+  },
+  {
+    "revision": "961bcbebe2f14bb709211de9fb64338d",
+    "url": "/portfolio/static/media/profile.961bcbeb.png"
+  }
+]);
